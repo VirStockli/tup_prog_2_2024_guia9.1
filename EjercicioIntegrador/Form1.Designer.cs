a7acef61106709d455675232a4109ce04882c9aa
VirStockli <virstockli@gmail.com>
2024-10-03 16:33:15 -0300

    Guia9 - Integrador

---
+++ EjercicioIntegrador/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace EjercicioIntegrador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.txbDNI = new System.Windows.Forms.TextBox();
+            this.txbNombre = new System.Windows.Forms.TextBox();
+            this.txbMail = new System.Windows.Forms.TextBox();
+            this.txbPatente = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnIniciarVtv = new System.Windows.Forms.Button();
+            this.btnVerVTV = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnIniciarVtv);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(402, 296);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Registro de VTV";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txbMail);
+            this.groupBox2.Controls.Add(this.txbNombre);
+            this.groupBox2.Controls.Add(this.txbDNI);
+            this.groupBox2.Location = new System.Drawing.Point(7, 20);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(389, 113);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Datos del propietario";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.txbPatente);
+            this.groupBox3.Controls.Add(this.dtpFecha);
+            this.groupBox3.Location = new System.Drawing.Point(7, 139);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(367, 90);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Datos del vehículo";
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Location = new System.Drawing.Point(124, 58);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(200, 22);
+            this.dtpFecha.TabIndex = 0;
+            // 
+            // txbDNI
+            // 
+            this.txbDNI.Location = new System.Drawing.Point(139, 21);
+            this.txbDNI.Name = "txbDNI";
+            this.txbDNI.Size = new System.Drawing.Size(100, 22);
+            this.txbDNI.TabIndex = 0;
+            // 
+            // txbNombre
+            // 
+            this.txbNombre.Location = new System.Drawing.Point(139, 47);
+            this.txbNombre.Name = "txbNombre";
+            this.txbNombre.Size = new System.Drawing.Size(243, 22);
+            this.txbNombre.TabIndex = 1;
+            // 
+            // txbMail
+            // 
+            this.txbMail.Location = new System.Drawing.Point(139, 74);
+            this.txbMail.Name = "txbMail";
+            this.txbMail.Size = new System.Drawing.Size(243, 22);
+            this.txbMail.TabIndex = 2;
+            // 
+            // txbPatente
+            // 
+            this.txbPatente.Location = new System.Drawing.Point(124, 32);
+            this.txbPatente.Name = "txbPatente";
+            this.txbPatente.Size = new System.Drawing.Size(100, 22);
+            this.txbPatente.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "DNI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Apellidos y Nombres";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(89, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Correo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Patente";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Fecha de hoy";
+            // 
+            // btnIniciarVtv
+            // 
+            this.btnIniciarVtv.Location = new System.Drawing.Point(101, 242);
+            this.btnIniciarVtv.Name = "btnIniciarVtv";
+            this.btnIniciarVtv.Size = new System.Drawing.Size(174, 41);
+            this.btnIniciarVtv.TabIndex = 2;
+            this.btnIniciarVtv.Text = "Iniciar Carga VTV";
+            this.btnIniciarVtv.UseVisualStyleBackColor = true;
+            // 
+            // btnVerVTV
+            // 
+            this.btnVerVTV.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerVTV.Location = new System.Drawing.Point(421, 231);
+            this.btnVerVTV.Name = "btnVerVTV";
+            this.btnVerVTV.Size = new System.Drawing.Size(105, 65);
+            this.btnVerVTV.TabIndex = 3;
+            this.btnVerVTV.Text = "Ver VTV Realizados";
+            this.btnVerVTV.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 321);
+            this.Controls.Add(this.btnVerVTV);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        public System.Windows.Forms.GroupBox groupBox1;
+        public System.Windows.Forms.TextBox txbPatente;
+        public System.Windows.Forms.DateTimePicker dtpFecha;
+        public System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox txbMail;
+        public System.Windows.Forms.TextBox txbNombre;
+        public System.Windows.Forms.TextBox txbDNI;
+        private System.Windows.Forms.Button btnIniciarVtv;
+        public System.Windows.Forms.Label label5;
+        public System.Windows.Forms.Label label4;
+        public System.Windows.Forms.Button btnVerVTV;
+    }
+}
+
---
+++ EjercicioIntegrador/FormVer.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace EjercicioIntegrador
+{
+    partial class FormVer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.txbVer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.Location = new System.Drawing.Point(209, 308);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(113, 37);
+            this.btnCerrar.TabIndex = 1;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // txbVer
+            // 
+            this.txbVer.Location = new System.Drawing.Point(12, 12);
+            this.txbVer.Multiline = true;
+            this.txbVer.Name = "txbVer";
+            this.txbVer.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txbVer.Size = new System.Drawing.Size(525, 290);
+            this.txbVer.TabIndex = 2;
+            // 
+            // FormVer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 357);
+            this.Controls.Add(this.txbVer);
+            this.Controls.Add(this.btnCerrar);
+            this.Name = "FormVer";
+            this.Text = "FormVer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        public System.Windows.Forms.Button btnCerrar;
+        public System.Windows.Forms.TextBox txbVer;
+    }
+}
